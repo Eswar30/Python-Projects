43328888fc983cf20acc47efcd80a023a2746095
Eswar_lazypanda <72182474+Eswar30@users.noreply.github.com>
2021-06-09 12:52:07 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,25 @@
+# Project 1
+# Guess The Gumber Game
+
+from random import randint
+
+value = randint(1,100)
+num1 = value
+guess1=10
+print("\n \t \t \tGame Starts Now\n\nYou have 9 guesses")
+
+while(guess1>0):
+    guess1 = guess1-1
+    userinput = int(input("Guess the number: "))
+    if(userinput < num1):
+        print("You entered smaller number\nTry again")
+        print("No of guesses left",guess1,"\n")
+        continue
+    elif(userinput > num1):
+        print("You entered greater number\nTry again")
+        print("No of guesses left",guess1,"\n")
+        continue
+    elif(userinput == num1):
+        print("Cograts you won the game")
+        print("whith",9-guess1,"chances")
+        break
---
+++ p4.py
@@ -0,0 +1,46 @@
+# project 4
+# Rock Paper Scissor
+
+import random
+a = ["rock","paper","scissor"]
+"""
+rock vs paper : paper
+rock vs scissor : rock
+paper vs scissor : scissor
+"""
+# iv = 0
+tie = 0
+no = 0
+n = 10
+print("\n \t \t Game starts now:\n")
+while(n>0):
+    n = n-1
+    # print(n)
+    c = random.choice(a)
+    i = input("Select: rock, paper or scissor\nYou choose: ").lower()
+    # print(a)
+    if i == c: 
+        print("Tie\n")
+        tie = tie + 1
+    elif i == "paper" and c =="rock":
+        print("You won\n")
+        no = no +1
+    elif i == "rock" and c == "scissor":
+        print("You won\n")
+        no = no +1
+    elif i == "scissor" and c == "paper":
+        print("You Won\n")
+        no = no +1
+
+    elif c =="paper"and i == "rock":
+        print("Opponent wins\n")
+    elif c =="scissor"and i == "paper": 
+        print("Opponent wins\n")
+    elif c =="rock"and i == "scissor" :
+        print("Opponent wins\n")
+    else:
+        print("Enter valied input")
+        # iv = iv + 1
+o = 10 - no - tie
+print("Game Over")    
+print(f"No of mathches you won : {no}\nNo of Tie brakes : {tie}\nNo of mathches opponent wins : {o}")
